275a39aea5785055909db05dd593f00741a7e90a
Nicholas NS <nikolasnanda@gmail.com>
2022-07-13 18:34:38 +0700

    add initial pages

---
+++ frontend/src/pages/History.jsx
@@ -0,0 +1,2 @@
+// This file is showing history of all LPR result with their
+// coresponding Base detected image, SRGAN result, and OCR result
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,4 @@
+// upload image in jpg,jpeg,png, or a video,
+// or even start capturing stream from cctv
+// then showing single result
+// or all detected plates if that is video input
