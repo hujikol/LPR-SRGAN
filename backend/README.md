3eecb79674ca670893aeb357e27a78e31506e672
hujikol <nikolasnanda@gmail.com>
2022-08-21 20:37:48 +0700

    Basic API

--- backend/README.md
+++ backend/README.md
@@ -6,7 +6,13 @@ In the project directory, you can run:
 
 Runs the Fast API server with uvicorn.\
 
-### `uvicorn main:app`
+### `uvicorn main:app --reload`
+
+> The command uvicorn main:app refers to:
+>
+> main: the file main.py (the Python "module").
+> app: the object created inside of main.py with the line app = FastAPI().
+> --reload: make the server restart after code changes. Only use for development.
 
 Open [http://localhost:8000](http://localhost:8000) to view the root in your browser, or use the router path to get another result.\
 
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,25 @@
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
+class FileType(str, Enum):
+    jpg = "jpg"
+    jpeg = "jpeg"
+    lenet = "lenet"
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/history/current-inference")
+async def read_current_history():
+    return {"history_id": "the current user"}
+
+@app.get("/history/all")
+async def read_all_history():
+    return {"history_id": ['id_list_and_data']}
+
+@app.get("/history/{inference_id}")
+async def specific_history(inference_id: int):
+    return {"history_id": inference_id}
